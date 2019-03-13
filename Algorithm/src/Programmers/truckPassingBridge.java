356ae4ff1217dd9311fa9d6929f131fbbe07c29c
Kim DongJin <41800328+kxv1379@users.noreply.github.com>
2019-03-13 14:29:14 +0900

    프로그래머스 - 다리를 지나는 트럭 풀이 (완료)

--- Algorithm/src/Programmers/truckPassingBridge.java
+++ Algorithm/src/Programmers/truckPassingBridge.java
@@ -1,9 +1,16 @@
 package Programmers;
-
+/**
+ * 
+  * @FileName : truckPassingBridge.java
+  * @Project : Algorithm
+  * @Date : 2019. 3. 12. 
+  * @Author : Kim DongJin
+  * @Comment : 프로그래머스 다리를 지나는 트럭 문제
+ */
 public class truckPassingBridge {
-	
+
 	static public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 1;
+        int answer = 0;
         int nowWeight=0;
         int front=0, nowNum=0;
         int[] nowTruckLocate=new int[truck_weights.length];
@@ -29,33 +36,28 @@ public class truckPassingBridge {
         	
         	else {
         		for(int j=front; j<front+nowNum; j++) {
-        			if(++nowTruckLocate[i]>=bridge_length) {
+        			if(++nowTruckLocate[j]>=bridge_length) {
         				nowWeight-=truck_weights[front];
         				front++;
         				nowNum--;
-        				
         			}
         		}
         		answer++;
         	}
         	
         	nowNum++;
-        	System.out.println(nowWeight+"  "+answer+" "+nowNum+" "+front);  
         	
         }
-        answer+=bridge_length-nowTruckLocate[front];
-        
-        
+        answer+=bridge_length-nowTruckLocate[truck_weights.length-1];
+           
         return answer;
     }
 
 	public static void main(String[] args) {
 		System.out.println(solution(2, 10, new int[]{7,4,5,6}));
-//		System.out.println(solution(100, 100, new int[]{10}));
-//		System.out.println(solution(100, 100, new int[]{10,10,10,10,10,10,10,10,10,10}));
-		
-	
-		
+		System.out.println(solution(100, 100, new int[]{10}));
+		System.out.println(solution(100, 100, new int[]{10,10,10,10,10,10,10,10,10,10}));
+			
 	}
 
 }
