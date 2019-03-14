93fc079cbdf6710ff30bc4693b63bb980aa18f1b
Kim DongJin <41800328+kxv1379@users.noreply.github.com>
2019-03-14 21:08:00 +0900

    프로그래머스 동적 계획법 - N으로 표현 문제 (완로)

--- Algorithm/src/Programmers/expressAsN.java
+++ Algorithm/src/Programmers/expressAsN.java
@@ -5,33 +5,39 @@ package Programmers;
   * @Project : Algorithm
   * @Date : 2019. 3. 13. 
   * @Author : Kim DongJin
-  * @Comment :프로그래머스 동적 계획법 - N으로 표현 문제
+  * @Comment :프로그래머스 동적 계획법 - N으로 표현 문제 (완로)
  */
 public class expressAsN {
 	
-	static int minCount=9, n, Num;
+	static int minCount=9,n, goalNum;
 
 	public static int solution(int N, int number) {
+        goalNum=number;
         n=N;
-        Num=number;
-        
-        recursion(N,1);
+        recursion(0,0);
         
         return (minCount==9)? -1 : minCount;
     }
 	
 	public static void recursion(int nowVal, int count) {
-		System.out.println(nowVal+" "+count);
-		if(nowVal!=Num && count<9 && count<minCount) {
-			recursion(nowVal+n,count+1);
-			recursion(nowVal-n,count+1);
-			recursion(nowVal*n,count+1);
-			recursion(nowVal/n,count+1);
-			recursion(Integer.parseInt(nowVal+""+n),count+1);
-			
-		}else if(nowVal==Num)
-			minCount=Math.min(count, minCount);
+		int nowNum=0;
 		
+		if(nowVal==goalNum) {
+			minCount=Math.min(count, minCount);
+			return;
+		}
+		
+		for(int i=0;i<9;i++) {
+			if(count>=minCount) 
+				return;
+			nowNum=Integer.parseInt(nowNum+""+n);
+			count++;
+			recursion(nowVal+nowNum,count);
+			recursion(nowVal-nowNum,count);
+			recursion(nowVal*nowNum,count);
+			recursion(nowVal/nowNum,count);
+
+		}
 		
 		
 	}
