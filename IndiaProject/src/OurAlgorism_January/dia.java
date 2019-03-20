e267c3851c86490a74d35a879f063782257d34a6
Kim DongJin <41800328+kxv1379@users.noreply.github.com>
2019-03-20 22:35:39 +0900

    다이아몬드 모양으로 별찍기

---
+++ IndiaProject/src/OurAlgorism_January/dia.java
@@ -0,0 +1,40 @@
+package OurAlgorism_January;
+import java.util.Scanner;
+
+/**
+ * 다이아몬드 만들기
+ * 
+ * @Package : OurAlgorism_January
+ * @FileName : dia.java
+ * @Author : KIM DONGJIN
+ * @date : 2018. 1. 22. 
+ *
+ */
+public class dia {
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int cen=n/2,sw=0,k=0;
+		for(int i=1;i<=n;i++) {
+			for(int j=1;j<=cen+1+k;j++) {
+					if(j>=cen+1-k)
+						System.out.print("*");
+					else
+						System.out.print(" ");
+			}
+			if(k==cen)
+				sw=1;
+			if(sw==0)
+				k++;
+			if(sw==1)
+				k--;
+			System.out.println();
+		}
+	}
+}
+				
+		
+
+	
+
+
