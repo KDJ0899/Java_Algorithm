91b85eacac5f32d697057213ceda38357f3c9401
KimDongJin <kxv1234@naver.com>
2019-11-27 16:03:22 +0900

    2020카카오 자물쇠와 열쇠 문제 풀이 (완료)

--- Algorithm/src/KaKaoTest_2020/LockAndKey.java
+++ Algorithm/src/KaKaoTest_2020/LockAndKey.java
@@ -1,6 +1,5 @@
 package KaKaoTest_2020;
 
-import java.util.Arrays;
 import java.util.HashSet;
 
 /**
@@ -8,12 +7,11 @@ import java.util.HashSet;
   * @Project : Algorithm
   * @Date : 2019. 11. 20.
   * @Author : Kim DongJin
-  * @Comment :2020카카오 자물쇠와 열쇠 문제 풀이 (진행중) 
+  * @Comment :2020카카오 자물쇠와 열쇠 문제 풀이 (완료) 
  */
 public class LockAndKey {
 	
 	static boolean finish = false;
-	static int[][] lock;
 	static int lockLength;
 	static int keyLength;
 	static int maxX,minX,minY,maxY;
@@ -26,8 +24,8 @@ public class LockAndKey {
 		
 		maxX=0;
 		minX=lockLength-1;
-		minY=0;
-		maxY=lockLength-1;
+		maxY=0;
+		minY=lockLength-1;
 		
 		checked = new HashSet<String>();
 	    
@@ -36,42 +34,53 @@ public class LockAndKey {
 				if(lock[i][j]==0) {
 					maxX=Math.max(j, maxX);
 					minX=Math.min(j, minX);
-					maxY=Math.max(j, maxY);
-					minY=Math.min(j, minY);
+					maxY=Math.max(i, maxY);
+					minY=Math.min(i, minY);
 				}
 			}
 		}
 		
-		
-		search(key,1,0,lock);
+		search(key,minX,minY,lock);
+		for(int i=0; i<3; i++) {
+			if(!finish) {
+				checked = new HashSet<String>();
+				key = rotate(key);
+				search(key,minX,minY,lock);
+			}
+		}
 		
 	    return finish;
 	}
 	 
 	public static void search(int[][] key, int x, int y,int[][] lock){
 		 
-		if(finish||checked.contains(x+" "+y)||y>maxY||x>maxX||y+keyLength<=minY||x+keyLength<=minX)
+		if(finish||checked.contains(x+" "+y)||y>minY||x>minX||y+keyLength<=maxY||x+keyLength<=maxX)
 			return;
+		
+		checked.add(x+" "+y);
 		
 		for(int i= 0; i<keyLength&&y+i<lockLength; i++) {
 			if(y+i>=0) {
 				for(int j=0; j<keyLength && x+j<lockLength; j++) {
 					if(x+j>=0) {
-						lock[y+i][x+j]+=key[i][j]; 
-//						lock[x+j][keyLength-1-y-i]+=key[i][j];
-//						lock[keyLength-1-x-j][keyLength-1-y-i]+=key[i][j];
-//						lock[keyLength-1-y-i][x+j]+=key[i][j];
+						if(lock[y+i][x+j]+key[i][j]!=1) {
+							search(key, x+1, y, lock);
+							search(key, x, y+1, lock);
+							search(key, x-1, y, lock);
+							search(key, x, y-1, lock);
+							return;
+						}
 					}
-						
 				}
 			}
 		}
-		for(int i=0; i<lockLength; i++)
-			System.out.println(Arrays.toString(lock[i]));
+		
+		finish=true;
+		return;
 		
 	}
 	
-	public static int[][] rotate(int[][] key, int x, int y,int[][] lock) {
+	public static int[][] rotate(int[][] key) {
 		
 		int[][] newArr = new int[keyLength][keyLength];
 		for(int i=0; i<keyLength; i++) {
