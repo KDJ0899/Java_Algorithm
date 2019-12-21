dcd01be76ed6cff3de8da6651c866135fe6eac7e
Kim DongJin <kxv1234@naver.com>
2019-12-21 16:20:36 +0900

    프로그래머스 예산 문제 풀이 완료

--- Algorithm/src/Programmers/Budget.java
+++ Algorithm/src/Programmers/Budget.java
@@ -9,13 +9,15 @@ import java.util.Arrays;
   * @Date : 2019. 12. 20. 
   * @Author : Kim DongJin
   * @Comment : 프로그래머스 예산 문제 풀이 완료
+  * 		
  */
 public class Budget {
 	
 	 public static int solution(int[] budgets, int M) {
 	        int answer = 0;
-	        long sum=0;
-	        int index = budgets.length-1,count=1, diff = 0, tmp;
+	        long sum=0; // sum은 모든 예산요청 값의 합이기 때문에 int범위를 넘을 수가 있음.
+	        int index = budgets.length-1, count=1, diff = 0, tmp; // index는 예산배열의 인덱스, count는 budgets.length - 1과 같음.
+	        													 // diff는 sum을 M으로 맞추기 위해 빼는 값. 
 	        
 	        Arrays.sort(budgets);
 	        
@@ -24,17 +26,16 @@ public class Budget {
 	        }
 	        
 	        if(sum<=M)
-	        	return budgets[budgets.length-1];
+	        	return budgets[budgets.length-1]; // 예산요청 값의 합이 예산 총액 M을 넘지 않을 경우 가장 큰값을 반환.
 	         
 	        int i = 1;
 	        while(true) {
-	        	System.out.println(count);
+
 	        	tmp = diff;
 	        	if(index != 0)
-	        		diff += (budgets[index] - budgets[index -1]) * count;
+	        		diff += (budgets[index] - budgets[index -1]) * count; // diff는 현재 index의 예산과 아래의 예산 값의 차이를 합산함.
 
 	        	if(index ==0 ||sum - diff <=M) {
-	        		int num = budgets[index];
 	        		sum -= tmp;
 	        		
 	        		while (true) {
