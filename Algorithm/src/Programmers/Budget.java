712c0e8e4e338a75017f1f90abde994a4eeecfbd
Kim DongJin <kxv1234@naver.com>
2019-12-21 16:12:49 +0900

    프로그래머스 예산 문제 풀이 완료

--- Algorithm/src/Programmers/Budget.java
+++ Algorithm/src/Programmers/Budget.java
@@ -8,13 +8,14 @@ import java.util.Arrays;
   * @Project : Algorithm
   * @Date : 2019. 12. 20. 
   * @Author : Kim DongJin
-  * @Comment : 프로그래머스 예산 문제 풀이
+  * @Comment : 프로그래머스 예산 문제 풀이 완료
  */
 public class Budget {
 	
-	 public int solution(int[] budgets, int M) {
+	 public static int solution(int[] budgets, int M) {
 	        int answer = 0;
-	        int sum=0,index = budgets.length-1,count=1, diff = 0, tmp;
+	        long sum=0;
+	        int index = budgets.length-1,count=1, diff = 0, tmp;
 	        
 	        Arrays.sort(budgets);
 	        
@@ -24,33 +25,42 @@ public class Budget {
 	        
 	        if(sum<=M)
 	        	return budgets[budgets.length-1];
-	        
-	        
+	         
+	        int i = 1;
 	        while(true) {
+	        	System.out.println(count);
 	        	tmp = diff;
-	        	diff += (budgets[index] - budgets[index -1]) * count;
-	        	
-	        	if(sum - diff <=M) {
-	        		int i=1;
+	        	if(index != 0)
+	        		diff += (budgets[index] - budgets[index -1]) * count;
+
+	        	if(index ==0 ||sum - diff <=M) {
 	        		int num = budgets[index];
 	        		sum -= tmp;
+	        		
 	        		while (true) {
 	        			if(sum - i *count <=M ) {
+
 	        				break;
 	        			}
+	        			i++;
 	        		}
 	        		
 	        		break;
 	        	}
+	        	count++;
+	        	index--;
 	        	
 	        }
+	        
+	        answer = budgets[index] - i;
 	        
 	        
 	        return answer;
 	    }
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		System.out.println(solution(new int[] {110,110,124,121}, 485));
 
 	}
 
