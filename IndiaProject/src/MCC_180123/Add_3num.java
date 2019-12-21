92a55302db1622ee4dbf66ff98df4264b4c2fa38
Kim DongJin <kxv1234@naver.com>
2019-12-21 19:14:18 +0900

    Remove IndiaProject Folder

--- IndiaProject/src/MCC_180123/Add_3num.java
+++
@@ -1,9 +0,0 @@
-package MCC_180123;
-
-public class Add_3num {
-	public static void main(String[] args) {
-		int a=10,b=20,c=30;
-		System.out.println(a+b+c);
-	}
-
-}
--- IndiaProject/src/MCC_180123/Evaluate.java
+++
@@ -1,8 +0,0 @@
-package MCC_180123;
-
-public class Evaluate {
-	public static void main(String[] args) {
-		System.out.println((5+3)/(2*4));
-	}
-
-}
