b8ad798610e537d9e045832ef35573bdd10cc528
Kim DongJin <kxv1234@naver.com>
2020-01-08 20:13:37 +0900

    프로그래머스 스택 탑 문제 풀이 완료

--- Algorithm/src/Programmers/Tower.java
+++ Algorithm/src/Programmers/Tower.java
@@ -1,13 +1,31 @@
 package Programmers;
+
+import java.util.Stack;
+
 /**
  * 
-  * @FileName : Top.java
+  * @FileName : Tower.java
   * @Project : Algorithm
   * @Date : 2020. 1. 8. 
   * @Author : Kim DongJin
   * @Comment : 프로그래머스 스택 탑 문제 풀이 완료
+  * 			solution은 내 풀이
+  * 			bestSolution 스택을 이용한 더 나은 풀이법
  */
 public class Tower {
+	
+	int idx;
+    int height;
+
+    public Tower(int idx, int height) {
+        this.idx = idx;
+        this.height = height;
+    }
+
+    @Override
+    public String toString() {
+        return "idx : " + idx + " height : " + height;
+    }
 	
 	 public int[] solution(int[] heights) {
 	  
@@ -25,6 +43,48 @@ public class Tower {
 	        	}
 	        }
 	        
+	        return answer;
+	    }
+	 
+	 
+	 class TowerClass {
+	        int idx;
+	        int height;
+
+	        public TowerClass(int idx, int height) {
+	            this.idx = idx;
+	            this.height = height;
+	        }
+
+	        @Override
+	        public String toString() {
+	            return "idx : " + idx + " height : " + height;
+	        }
+	    }
+
+	    public int[] bestSolution(int[] heights) {
+	        Stack<TowerClass> st = new Stack<>();
+	        int[] answer = new int[heights.length];
+
+	        for (int i = 0; i < heights.length; i++) {
+	        	TowerClass tower = new TowerClass(i + 1, heights[i]);
+	            int receiveIdx = 0;
+
+	            while (!st.isEmpty()) {
+	            	TowerClass top = st.peek();
+
+	                if (top.height > tower.height) {
+	                    receiveIdx = top.idx;
+	                    break;
+	                }
+
+	                st.pop();
+	            }
+
+	            st.push(tower);
+	            answer[i] = receiveIdx;
+	        }
+
 	        return answer;
 	    }
 
