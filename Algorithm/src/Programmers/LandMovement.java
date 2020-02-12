618814a63c9200b8b94f39d411629d19e5d8fe81
Kim DongJin <kxv1234@naver.com>
2020-02-12 18:44:25 +0900

    프로그래머스 지형이동 문제.

--- Algorithm/src/Programmers/LandMovement.java
+++ Algorithm/src/Programmers/LandMovement.java
@@ -1,7 +1,14 @@
 package Programmers;
 
 import java.util.List;
-
+/**
+ * 
+  * @FileName : LandMovement.java
+  * @Project : Algorithm
+  * @Date : 2020. 2. 10. 
+  * @Author : Kim DongJin
+  * @Comment :프로그래머스 지형이동 문제.
+ */
 public class LandMovement {
 	public int solution(int[][] land, int height) {
         int answer = 0;
