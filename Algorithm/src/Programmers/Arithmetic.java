4def02091ad33c3cac5e0c14af1d9b12930ccef7
Kim DongJin <kxv1234@naver.com>
2020-02-25 18:53:03 +0900

    프로그래머스 사칙연산 문제 풀이

--- Algorithm/src/Programmers/Arithmetic.java
+++ Algorithm/src/Programmers/Arithmetic.java
@@ -1,92 +1,86 @@
 package Programmers;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * 
-  * @FileName : Arithmetic.java
-  * @Project : Algorithm
-  * @Date : 2020. 2. 24. 
-  * @Author : Kim DongJin
-  * @Comment : 프로그래머스 사칙연산 문제 풀이 (정확성 통과 , all + 문제 해결.)
+ * @FileName : Arithmetic.java
+ * @Project : Algorithm
+ * @Date : 2020. 2. 24.
+ * @Author : Kim DongJin
+ * @Comment : 프로그래머스 사칙연산 문제 풀이
  */
 public class Arithmetic {
-	static int answer = Integer.MIN_VALUE;
+	static int[][] dp;
+
 	public static int solution(String arr[]) {
-		
-		List<String> operators = new ArrayList<String>();
-		List<Integer> numbers = new ArrayList<Integer>();
-		int num = Integer.parseInt(arr[0]);
-		
-		for(int i=0; i<arr.length/2; i++) {
-			
-			if(arr[i*2+1].equals("+")) {
-				num += Integer.parseInt(arr[i*2+2]);
+		int answer = 1;
+		int N = arr.length;
+
+		dp = new int[N][N];
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				dp[i][j] = Integer.MIN_VALUE;
 			}
-			else {
-				if(num!=0) {
-					numbers.add(num);
-					num = 0;
-					if(operators.size()!=0)
-						operators.add("+");
-				}
-				numbers.add(Integer.parseInt(arr[i*2+2]));
-				operators.add(arr[i*2+1]);
+			if (i % 2 == 0) {
+				dp[i][i] = Integer.parseInt(arr[i]);
+
 			}
 		}
-		
-		if(num!=0) {
-			numbers.add(num);
-			if(operators.size()!=0) {
-				operators.add("+");
-			}
-			else
-				answer=num;
+
+		for (int i = 0; i < N; i++) {
+			System.out.println(Arrays.toString(dp[i]));
 		}
-		
-		for(int i=0; i<operators.size(); i++) {
-			calculate(operators, numbers, i);
+		answer = solve(arr, 0, N - 1);
+		System.out.println();
+		for (int i = 0; i < N; i++) {
+			System.out.println(Arrays.toString(dp[i]));
 		}
-		
+
 		return answer;
 	}
-	
-	public static void calculate(List<String> operators, List<Integer> numbers,int index) {
-		
-		int result;
-		List<String> newOperators = new ArrayList<String>();
-		newOperators.addAll(operators);
-		
-		List<Integer> newNumbers = new ArrayList<Integer>();
-		newNumbers.addAll(numbers);
-		
-		if(operators.get(index).equals("+")) {
-			result=numbers.get(index)+numbers.get(index+1);
+
+	public static int solve(String[] arr, int a, int b) {
+		System.out.println(a+"   "+b);
+		 
+		if (dp[a][b] != Integer.MIN_VALUE) {
+			return dp[a][b];
 		}
-		else{
-			result=numbers.get(index)-numbers.get(index+1);
+
+		if (a - 1 >= 1 && arr[a - 1].equals("-")) {
+			int result = Integer.MAX_VALUE;
+			for (int i = a; i < b; i += 2) {
+				if (arr[i + 1].equals("-")) {
+					result = Math.min(result, solve(arr, a, i) - solve(arr, i + 2, b));
+				} else {
+					result = Math.min(result, solve(arr, a, i) + solve(arr, i + 2, b));
+				}
+			}
+			dp[a][b] = result;
+		} else {
+			int result = Integer.MIN_VALUE;
+			for (int i = a; i < b; i += 2) {
+				if (arr[i + 1].equals("-")) {
+					result = Math.max(result, solve(arr, a, i) - solve(arr, i + 2, b));
+				} else {
+					result = Math.max(result, solve(arr, a, i) + solve(arr, i + 2, b));
+				}
+			}
+			dp[a][b] = result;
 		}
-		
-		newNumbers.set(index, result);
-		newNumbers.remove(index+1);
-		newOperators.remove(index);
-			
-		for(int i=0; i<newOperators.size(); i++) {
-			calculate(newOperators, newNumbers, i);
-		}
-		
-		if(newOperators.size()==0) {
-			answer = Math.max(newNumbers.get(0), answer);
-		}
+		System.out.println(dp[a][b]);
+		return dp[a][b];
 	}
 
 	public static void main(String[] args) {
-		System.out.println(solution(new String[] {"1","-","5","-","8","-","4"}));
-		System.out.println(solution(new String[] {"4", "+", "5", "+", "6", "-", "5", "+", "4", "+", "2", "+", "1", "-", "5", "+", "6", "+", "7", "-", "8", "-", "4"}));
-		System.out.println(solution(new String[] {"4", "+", "5", "+", "6", "+", "5", "+", "4", "+", "2", "+", "1", "+", "5", "+", "6", "+", "7", "+", "8", "+", "4"}));
+		System.out.println(solution(new String[] { "1", "-", "3", "+", "5", "-", "8" }));
+//		System.out.println(solution(new String[] { "4", "+", "5", "+", "6", "-", "5", "+", "4", "+", "2", "+", "1", "-",
+//				"5", "+", "6", "+", "7", "-", "8", "-", "4" }));
+//		System.out.println(solution(new String[] { "4", "+", "5", "+", "6", "+", "5", "+", "4", "+", "2", "+", "1", "+",
+//				"5", "+", "6", "+", "7", "+", "8", "+", "4" }));
 		List<Integer> list = new ArrayList<Integer>();
-		
 
 	}
 
