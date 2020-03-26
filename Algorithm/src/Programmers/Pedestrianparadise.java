9f4d7844f60a14b76044b8eb0d35fd2c65845829
Kim DongJin <kxv1234@naver.com>
2020-03-26 16:09:05 +0900

    2017 카카오 예선 보행자 천국 문제 풀이

---
+++ Algorithm/src/Programmers/Pedestrianparadise.java
@@ -0,0 +1,61 @@
+package Programmers;
+/**
+ * 
+  * @FileName : Pedestrianparadise.java
+  * @Project : Algorithm
+  * @Date : 2020. 3. 26. 
+  * @Author : Kim DongJin
+  * @Comment : 2017 카카오 예선 보행자 천국 문제 풀이
+ */
+public class Pedestrianparadise {
+
+	static int MOD = 20170805;
+	static int M,N;
+	public static int solution(int m, int n, int[][] cityMap) {
+		int answer = 0;
+		int [][] driveMap = new int[m][n];
+		M = m-1; 
+		N= n-1;
+		answer = drive(0, 0, cityMap, driveMap, Direction.RIGHT);
+		return answer%MOD;
+	}
+	
+	public static int drive(int x, int y,int[][] cityMap,int[][] driveMap, Direction direction) {
+		if(cityMap[y][x] == 1) return 0;
+		if(M==y&&N==x) return 1;
+		if(cityMap[y][x]==2) {
+			driveMap[y][x] = direction.move(x, y, cityMap, driveMap);
+		}
+		else {
+			if(driveMap[y][x] !=0&& cityMap[y][x]==0) return driveMap[y][x];
+			driveMap[y][x]+= Direction.DOWN.move(x, y, cityMap, driveMap);
+			driveMap[y][x]+= Direction.RIGHT.move(x, y, cityMap, driveMap);
+		}
+		
+		return driveMap[y][x];
+	}
+	
+	enum Direction{
+		DOWN{
+			@Override
+			public int move(int x, int y,int[][] cityMap,int[][] driveMap) {
+				if(y+1>M)
+					return 0;
+				int result = drive(x, y+1, cityMap, driveMap, Direction.DOWN);
+				return result;
+			}
+		},
+		RIGHT{
+			@Override
+			public int move(int x, int y,int[][] cityMap,int[][] driveMap) {
+				if(x+1>N)
+					return 0;
+				int result = drive(x+1, y, cityMap, driveMap, Direction.RIGHT);
+				return result;
+			}
+		};
+		
+		public abstract int move(int x,int y,int[][] cityMap,int[][] driveMap);
+	}
+
+}
