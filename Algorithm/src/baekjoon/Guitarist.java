3d7691bf9743eff718f92fe17f360f2d6c2bbd6c
Kim DongJin <kxv1234@naver.com>
2020-03-27 15:26:25 +0900

    백준 1495번 기타리스트 문제 풀이 완료.

--- Algorithm/src/baekjoon/Guitarist.java
+++ Algorithm/src/baekjoon/Guitarist.java
@@ -1,13 +1,17 @@
 package baekjoon;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.Set;
 /**
  * 
   * @FileName : Guitarist.java
   * @Project : Algorithm
   * @Date : 2020. 3. 27. 
   * @Author : Kim DongJin
-  * @Comment : 백준 1495번 기타리스트 문제 풀이.
+  * @Comment : 백준 1495번 기타리스트 문제 풀이 완료.
  */
 public class Guitarist {
 
@@ -18,20 +22,35 @@ public class Guitarist {
 		int N = sc.nextInt();
 		int S = sc.nextInt();
 		int M = sc.nextInt();
-		int[] V = new int[N];
+		int[] V = new int[N+1];
 		
-		for(int i=0; i<N; i++) {
+		int answer =-1;
+		
+		Map<Integer, Set<Integer>> results = new HashMap<Integer, Set<Integer>>();
+		Set<Integer> volumes = new HashSet<Integer>();
+		
+		volumes.add(S);
+		results.put(0, volumes);
+		
+		for(int i=1; i<=N; i++) {
 			V[i] = sc.nextInt();
+			volumes = new HashSet<Integer>();
+			
+			for(int volume : results.get(i-1)) {
+				if(volume + V[i]<=M)
+					volumes.add(volume + V[i]);
+				if(volume - V[i]>=0)
+					volumes.add(volume - V[i]);
+			}
+			results.put(i, volumes);
 		}
 		
-		System.out.println(controlVolume(V, S, M, 0));
+		for(int volume : results.get(N)) {
+			answer = Math.max(answer, volume);
+		}
+		
+		System.out.println(answer);
+		
 	}
 	
-	public static int controlVolume(int[] volumes, int nowVolume, int max, int i) {
-		if(nowVolume>max||nowVolume<0) return -1;
-		if(i==volumes.length) return nowVolume;
-		
-		return Math.max(controlVolume(volumes, nowVolume+volumes[i], max, i+1), controlVolume(volumes, nowVolume-volumes[i], max, i+1));
-	}
-
 }
