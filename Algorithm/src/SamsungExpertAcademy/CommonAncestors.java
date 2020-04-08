e6325d5e0f600e8fff00eff8a2462a830fc4b47f
Kim DongJin <kxv1234@naver.com>
2020-04-08 17:01:19 +0900

    Samsung Expert Academy  D5 공통조상 문제 풀이 완료

--- Algorithm/src/SamsungExpertAcademy/CommonAncestors.java
+++ Algorithm/src/SamsungExpertAcademy/CommonAncestors.java
@@ -1,16 +1,16 @@
 package SamsungExpertAcademy;
 
-import java.util.HashMap;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Scanner;
+import java.util.Set;
 
 /**
  * 
   * @FileName : CommonAncestors.java
   * @Project : Algorithm
-  * @Date : 2019. 12. 17. 
+  * @Date : 2020. 4. 8. 
   * @Author : Kim DongJin
-  * @Comment : Samsung Expert Academy  D5 공통조상 문제 풀이 
+  * @Comment : Samsung Expert Academy  D5 공통조상 문제 풀이 완료
  */
 public class CommonAncestors {
 
@@ -18,36 +18,49 @@ public class CommonAncestors {
 		
 		Scanner sc = new Scanner(System.in);
 		
-		HashMap<Integer, int[]> tree = new HashMap<Integer, int[]>();
+		int T = sc.nextInt(), V, E, x, y, p, c;
+		int[] parents, subNodeNum;
+		Set<Integer> ancestors;
 		
-		int n = sc.nextInt();
-		int v,e,x,y,parent,child;
-		int[] arr;
-		for(int i=0; i<n; i++) {
-			v = sc.nextInt();
-			e = sc.nextInt();
+		for(int test_case = 1; test_case<=T; test_case++) {
+			V = sc.nextInt();
+			E = sc.nextInt();
 			x = sc.nextInt();
 			y = sc.nextInt();
+			parents = new int[V+1];
+			subNodeNum = new int[V+1];
+			subNodeNum[1] = 1;
+			parents[1] = 0;
+			ancestors = new HashSet<Integer>();
 			
-			for(int j=0; j<e; j++) {
-				parent = sc.nextInt();
-				child = sc.nextInt();
+			for(int i=0; i<E; i++) {
+				p = sc.nextInt();
+				c = sc.nextInt();
 				
-				if(!tree.containsKey(parent))
-					tree.put(parent, new int[] {child,0});
-				else {
-					arr = tree.get(parent);
-					arr[1]= child;
-					tree.put(parent,arr);
-				}
+				parents[c] = p;
+				subNodeNum[c]++;
+				arrange(parents, subNodeNum, c, subNodeNum[c]);
+			}
+			while(x!=1) {
+				x = parents[x];
+				ancestors.add(x);
+			}
+			while(y!=1) {
+				y = parents[y];
+				if(ancestors.contains(y))
+					break;
+				ancestors.add(y);
 			}
 			
+			System.out.println("#"+test_case+" "+y+" "+subNodeNum[y]);
 		}
 	}
-//	
-//	public List<Integer> search(){
-//		
-//		
-//	}
+	
+	public static void arrange(int[] parents, int[] subNodeNum, int index, int value) {
+		while(index>1) {
+			index = parents[index];
+			subNodeNum[index]+= value;
+		}
+	}
 
 }
